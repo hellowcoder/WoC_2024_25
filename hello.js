b77c0c3d197b2235c8f1ca428a7a8033960563ec
hellowcoder <iitianias@gmail.com>
2024-12-05 21:07:33 +0530

    splice

--- hello.js
+++ hello.js
@@ -1,2 +1,4 @@
-fullname="Sandeep KUmar"
-console.log(fullname)
+const read=require('prompt-sync')()
+let b = [2,4,5,6,7]
+b.splice(2,0,100,104) //index 2 ko remand pe liya uske bad ke 0 element ko delete kiya then index 2 se hi 100,104 ko insert kar diya.
+console.log(b)  
